76b4652f83012374cf6a19e8373fbce5adb25dba
Aatmiya Silwal <aatmiyasilwal1@gmail.com>
2024-11-04 00:54:08 +0800

    Functionality to save in downloads using platform

--- doc2pdf.py
+++ doc2pdf.py
@@ -1,5 +1,6 @@
 import os
-from docx2pdf import convert
+import subprocess
+import shutil
 import platform
 
 def get_downloads_folder():
@@ -11,15 +12,9 @@ def get_downloads_folder():
 def convert_one_file(filename):
     downloads_folder = get_downloads_folder()
     pdf_filename = os.path.join(downloads_folder, f"{os.path.basename(filename)[:-5]}.pdf")
-    
-    convert(filename, pdf_filename)
-    
-    print(f"Converted {filename} to {pdf_filename}")
-    
-    if os.path.exists(filename):
-        os.remove(filename)
-        print(f"Deleted {filename}")
-    else:
-        print(f"Could not delete {filename}")
 
-convert_one_file("abc.docx")
+    subprocess.run(['soffice', '--headless', '--convert-to', 'pdf', filename, '--outdir', 'tmp'], check=True)
+    os.rename(f'tmp/{filename[:-5]}.pdf', pdf_filename)
+    shutil.rmtree("tmp")
+
+convert_one_file("WhatsApp Image 2024-10-05 at 23.06.02.jpeg")
